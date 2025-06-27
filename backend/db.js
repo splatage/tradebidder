bf5dd3eb149d2a573b15d52efbcfae5ced0d21b7
Splatage.com <81358782+splatage@users.noreply.github.com>
2025-06-27 20:59:15 +1200

    Create db.js

---
+++ backend/db.js
@@ -0,0 +1,7 @@
+const pool = mariadb.createPool({
+  host: 'localhost',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  connectionLimit: 10,
+});
