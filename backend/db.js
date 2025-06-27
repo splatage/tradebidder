037ba859cd6de6669c75ea3a0587fcd654ec2a5e
Splatage.com <81358782+splatage@users.noreply.github.com>
2025-06-27 22:48:00 +1200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mariadb = require('mariadb'); 
 
 const pool = mariadb.createPool({
-  host: 'localhost',
+  host: process.env.DB_HOST,,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
