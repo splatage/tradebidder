460ba817022f9e875ef9fd0d226ce4387bafc901
Splatage.com <81358782+splatage@users.noreply.github.com>
2025-06-27 18:29:23 +1200

    Initial commit

---
+++ sql/migrate_up.sql
@@ -0,0 +1,10 @@
+-- === MIGRATION SCRIPT ===
+SOURCE users.sql;
+SOURCE regions.sql;
+SOURCE jobs.sql;
+SOURCE bids.sql;
+SOURCE reviews.sql;
+SOURCE messages.sql;
+SOURCE notifications.sql;
+SOURCE portfolio_items.sql;
+SOURCE flags.sql;
